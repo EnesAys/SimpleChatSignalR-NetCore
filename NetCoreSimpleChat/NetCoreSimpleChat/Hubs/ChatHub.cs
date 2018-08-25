5ca0c1cc34adf35b07b2338e0bf7511b185e3ed9
Enes Aysan <enesaysan8@gmail.com>
2018-08-25 23:43:54 +0300

    Initial Commit

---
+++ NetCoreSimpleChat/NetCoreSimpleChat/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreSimpleChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public void SendMessage(string user, string message)
+        {
+            if (String.IsNullOrEmpty(user))
+            {
+                user = "DefaultGuest";
+            }
+            Clients.All.SendAsync("Messages", user, message);
+        }
+    }
+}
